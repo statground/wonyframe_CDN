0a77b475666078b8bc733154ee89cdcba0f6322a
Jaeseong Yoo <praster1@gmail.com>
2023-10-12 19:11:14 +0900

    TailwindCSS configuration

---
+++ scripts/common/tailwind_config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+	theme: {
+		screens: {
+			'xl': {'max': '1536px'},
+			'lg': {'max': '1280px'},
+			'md': {'max': '1024px'},
+			'sm': {'max': '768px'},
+			'xs': {'max': '640px'},
+		},
+		extend: {
+			colors: {
+				backgroundblack: '#ffffff'
+			}
+		}
+	}
+}
