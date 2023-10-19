c96a812ae38d89618e9f0373e3dda1795038c6b7
Jaeseong Yoo <praster1@gmail.com>
2023-10-20 05:16:58 +0900

    회원가입 - 이미 가입된 메일인지 확인

---
+++ scripts/account/signup/input_checker.js
@@ -0,0 +1,39 @@
+// 이메일 입력 값에 따른 이벤트
+function input_checker() {
+	let checker_email = false
+
+	let class_msg = "text-[#FA5252] text-[12px] font-[500] mb-[12px]"
+
+
+	// 오류 메시지
+	function Div_desc_err_msg(props) { 
+		return (
+			<span class={props.class}>{props.text}</span>
+		) 
+	}
+
+	
+	// 이메일을 입력하지 않음
+	if (email_form_check("txt_email") == "NOT EXIST") {
+		ReactDOM.render(<Div_desc_err_msg class={class_msg} text="이메일을 입력해주세요." />, document.getElementById("desc_email_msg"))
+		
+	// 이메일 형식이 잘못되었음
+	} else if (email_form_check("txt_email") == "FAILED") {
+		ReactDOM.render(<Div_desc_err_msg class={class_msg} text="이메일 형식이 잘못되었습니다." />, document.getElementById("desc_email_msg"))
+
+	// 이메일 형식이 올바르게 입력되었음
+	} else {
+		ReactDOM.render(<Div_desc_err_msg class={"hidden"} text="" />, document.getElementById("desc_email_msg"))
+		checker_email = true
+
+	}
+
+
+	if (checker_email) {
+		ReactDOM.render(<Div_btn_submit class_btn={class_btn_enabled} function={() => click_login_email_submit()} />, document.getElementById("btn_submit"))
+
+	} else {
+		ReactDOM.render(<Div_btn_submit class_btn={class_btn_disabled} function={null} />, document.getElementById("btn_submit"))
+
+	}
+}
