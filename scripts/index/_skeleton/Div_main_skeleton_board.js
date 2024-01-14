8d790bb2150e710b80180496c3d6a81b1786bde0
Jaeseong Yoo <praster1@gmail.com>
2024-01-14 20:40:48 +0900

    skeleton

---
+++ scripts/index/_skeleton/Div_main_skeleton_board.js
@@ -0,0 +1,9 @@
+function Div_main_skeleton_board() {
+	return (
+		<div class="flex flex-col justify-center items-start w-full h-fit border-2 border-gray-200 rounded-xl space-y-2 animate-pulse p-[16px]">
+			<div class="w-full max-w-[360px] h-2 bg-gray-300 rounded-full"></div>
+			<div class="w-full max-w-[375px] h-2 bg-gray-300 rounded-full"></div>
+			<div class="w-full max-w-[330px] h-2 bg-gray-300 rounded-full"></div>
+		</div>
+	)
+}
---
+++ scripts/index/_skeleton/Div_main_skeleton_cols3.js
@@ -0,0 +1,18 @@
+function Div_main_skeleton_cols3() {
+	function Div_main_skeleton_sub() {
+		return (
+			<div class="flex flex-row justify-center items-center w-full">
+				<div class="grid grid-cols-1 content-between bg-gray-300 rounded-xl w-full min-w-[160px] max-w-[360px] h-[250px]">
+				</div>
+			</div>
+		)
+	}
+
+	return (
+		<div class="grid grid-cols-3 justify-center items-center w-full gap-[12px] animate-pulse md:grid-cols-1">
+			<Div_main_skeleton_sub /> 
+			<Div_main_skeleton_sub /> 
+			<Div_main_skeleton_sub /> 
+		</div>
+	)
+}
