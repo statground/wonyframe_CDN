f9be3545379e004de5ddedaaa0f5a1095538f6a6
Jaeseong Yoo <praster1@gmail.com>
2024-01-16 01:32:26 +0900

    bug fix

---
+++ scripts/index/_skeleton/Div_main_skeleton_card_list_20240116_0132.js
@@ -0,0 +1,17 @@
+function Div_main_skeleton_card_list(props) {
+	function Div_main_skeleton_sub() {
+		return (
+			<div class="flex flex-row justify-center items-center w-full">
+				<div class="grid grid-cols-1 content-between bg-gray-300 rounded-xl w-full min-w-[160px] max-w-[360px] h-[250px]">
+				</div>
+			</div>
+		)
+	}
+
+	return (
+		<div class={"grid grid-cols-" + props.cols + " justify-center items-center w-full gap-[12px] animate-pulse md:grid-cols-1"}>
+			<Div_main_skeleton_sub /> 
+			<Div_main_skeleton_sub /> 
+		</div>
+	)
+}
